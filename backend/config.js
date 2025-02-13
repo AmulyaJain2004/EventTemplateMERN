744c17fc214a7c6c8e2e791c7935433d96a598da
AmulyaJain2004 <jainamulyawin@gmail.com>
2025-02-13 22:16:25 +0530

    Resolved dependency issues

--- backend/config.js
+++ backend/config.js
@@ -2,4 +2,4 @@ export const PORT = 5555;
 
 // Username: root
 // Password: root
-export const mongoDBURL = 'mongodb+srv://root:root@events.u2d7w.mongodb.net/?retryWrites=true&w=majority&appName=events'
+export const mongoDBURL = 'mongodb+srv://root:root@hackathon.u2d7w.mongodb.net/?retryWrites=true&w=majority&appName=hackathon'
